6f65b0085092b74b765b28324ed312519d5bdaa5
r17x <ri7nz@evilfactory.id>
2022-08-22 18:46:08 +0800

    chore(utils): add util function for fp.js

--- src/fp.js
+++ src/fp.js
@@ -5,24 +5,41 @@
  * @returns {void}
  */
 export const noOp = () => {};
-
+export const eq = (a, b) => a === b;
+export const notEq = (a, b) => a !== b;
+export const length = (a) => a.length;
 /**
-  * @type <A=unknown, B=unknown,>(condition: boolean, whenTrue: (...a: A) => B, whenFalse: (...b: A) => B) => (...args: A) => B 
-  */
+ * @type <A=unknown, B=unknown,>(condition: boolean, whenTrue: (...a: A) => B, whenFalse: (...b: A) => B) => (...args: A) => B
+ */
 export const ifElse =
   (condition, whenTrue, whenFalse) =>
   (...args) =>
     (condition ? whenTrue : whenFalse)(...args);
 /**
-  * @type <A,B,>(f: A => B, x: A) => B
-  */
-export const fx = (f, x) => f(x);
-/**
-  * @type <A=unknown,B,>(x: A, f: A => B, ) => B
-  */
-export const fxFlip = (x, f) => fx(f, x/**@type number*/);
+ * @type <A,B,>(f: A => B, x: A) => B
+ */
+export const fx = (f, x) => log(f(x));
 
-export const reduce = (f, b, a) => a.reduce(f, b);
+export const flip = (f) => (a, b) => f(b, a);
+
+const log = (a) => {
+  notEq(String(process.env.NODE_ENV).toLowerCase(), "production") &&
+    console.log(a);
+  return a;
+};
+/**
+ * @type <A=unknown,B,>(x: A, f: A => B, ) => B
+ */
+export const fxFlip = flip(fx);
+
+export const curry = (f, ...a) =>
+  eq(length(f), length(a)) ? f(...a) : (...b) => curry(f, ...a, ...b);
+
+export const reduce = curry((f, b, a) => a.reduce(f, b));
+
+export const map = curry((f, a) => a.map((x) => f(x)));
+
+export const mapWithIndex = curry((f, a) => a.map((b, c) => f(b, c)));
 
 export const compose =
   (...args) =>
